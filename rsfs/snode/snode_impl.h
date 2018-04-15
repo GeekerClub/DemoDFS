c3e5567b95b136afda0d5ae572d95592324dda22
An Qin <anqin.qin@gmail.com>
2018-04-15 13:10:07 +0800

    fix the snode imple

--- rsfs/snode/snode_impl.h
+++ rsfs/snode/snode_impl.h
@@ -2,23 +2,29 @@
 // Author: An Qin (anqin.qin@gmail.com)
 //
 // Description:
+//
 
 #ifndef RSFS_SNODE_SNODE_IMPL_H
 #define RSFS_SNODE_SNODE_IMPL_H
 
-#include "toft/base/closure.h"
-#include "toft/thread/thread_pool.h"
+#include "toft/base/scoped_ptr.h"
+#include "toft/system/threading/event.h"
+#include "toft/system/threading/mutex.h"
+#include "toft/system/threading/thread_pool.h"
 
-#include "rsfs/master/master_client.h"
 #include "rsfs/proto/snode_info.pb.h"
 #include "rsfs/proto/snode_rpc.pb.h"
 #include "rsfs/proto/status_code.pb.h"
 
 namespace rsfs {
+
+namespace master {
+class MasterClient;
+}
+
 namespace snode {
 
-class BlockManager;
-class BlockStream;
+class WorkerManager;
 
 class SNodeImpl {
 public:
@@ -26,10 +32,10 @@ public:
         kNotInited = kSNodeNotInited,
         kIsIniting = kSNodeIsIniting,
         kIsBusy = kSNodeIsBusy,
-        kIsRunning = kSNodeIsRunning
+        kRunning = kSNodeIsRunning
     };
-    SNodeImpl(const SNodeInfo& snode_info,
-              master::MasterClient* master_client);
+
+    SNodeImpl(const SNodeInfo& node_info);
     ~SNodeImpl();
 
     bool Init();
@@ -40,37 +46,30 @@ public:
 
     bool Report();
 
-    void OpenData(const OpenDataRequest* request,
-                  OpenDataResponse* response,
-                  google::protobuf::Closure* done);
+    void TrigerReportByEvent();
 
-    void CloseData(const CloseDataRequest* request,
-                  CloseDataResponse* response,
-                  google::protobuf::Closure* done);
+    bool OpenData(const OpenDataRequest* request,
+                       OpenDataResponse* response);
 
-    void WriteData(const WriteDataRequest* request,
-                   WriteDataResponse* response,
-                   google::protobuf::Closure* done);
+    bool CloseData(const CloseDataRequest* request,
+                          CloseDataResponse* response);
 
-    void ReadData(const ReadDataRequest* request,
-                   ReadDataResponse* response,
-                   google::protobuf::Closure* done);
+    bool WriteData(const WriteDataRequest* request,
+                          WriteDataResponse* response);
+
+    bool IsIdle();
 
 private:
-    bool ReadDataSequencial(BlockStream* stream, uint64_t size,
-                            ReadDataResponse* response);
-    bool ReadDataRandom(BlockStream* stream, uint64_t size, uint64_t offset,
-                        ReadDataResponse* response);
+    mutable toft::Mutex m_status_mutex;
 
-private:
-    SNodeInfo m_snode_info;
     SNodeStatus m_status;
     uint64_t m_this_sequence_id;
 
-    master::MasterClient* m_master_client;
-    toft::scoped_ptr<BlockManager> m_block_manager;
-    toft::scoped_ptr<ThreadPool> m_thread_pool;
+    toft::scoped_ptr<master::MasterClient> m_master_client;
+    toft::scoped_ptr<toft::ThreadPool> m_thread_pool;
+    toft::AutoResetEvent m_report_event;
 };
+
 
 } // namespace snode
 } // namespace rsfs
